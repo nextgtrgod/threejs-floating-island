fb7893f686e8b28f050694467af52688c59b1bb3
nextgtrgod <KYakovlev7@gmail.com>
2020-08-09 17:34:48 +0300

    deploy log fix

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ git add dist --force
 git commit -m 'deploy'
 git push origin `git subtree split --prefix dist master`:gh-pages --force
 
-echo 'Deployed: \033[1;32;4mhttps://nextgtrgod.github.io/threejs-lowpoly-world/\033[0m'
+echo 'Deployed: \033[1;32;4mhttps://nextgtrgod.github.io/threejs-floating-island/\033[0m'
 echo ''
